44041974ea89446dd8b485a2c628084ca7b8b80f
Kyra Christensen <christensen.kyral@gmail.com>
2022-05-14 01:54:04 -0700

    setup the list.jsx file, added some css styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import styles from '../App.css';
 
 export default function Header() {
   return (
-    <header>
+    <header className={styles.header}>
       <h1>Studio Ghibli Film Collection</h1>
     </header>
   )
